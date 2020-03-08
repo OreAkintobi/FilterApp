10a67c883faff5dd54d20cfde930f4440468ba80
OreAkintobi <oreakintobi@gmail.com>
2020-03-08 17:20:34 +0100

    Implemented exception handling for network connection

--- app/src/main/java/com/oreakintobi/oreakintobi/ui/FilterFragment.kt
+++ app/src/main/java/com/oreakintobi/oreakintobi/ui/FilterFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -16,6 +17,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.downloader.PRDownloader
 import com.downloader.PRDownloaderConfig
 import com.oreakintobi.oreakintobi.*
+import com.oreakintobi.oreakintobi.data.NetworkChecker.isNetworkAvailable
 import com.oreakintobi.oreakintobi.databinding.FragmentFilterBinding
 import com.oreakintobi.oreakintobi.repositories.FilterRepository
 import com.oreakintobi.oreakintobi.utils.hide
@@ -61,6 +63,12 @@ class FilterFragment : Fragment(), DataLoadingListener {
             }
             binding.recyclerView.adapter = adapter
         })
+
+        if (!isNetworkAvailable(context!!)!!) {
+            binding.rootLayout.snackbar("No internet connection")
+            Toast.makeText(context, "No internet connection", Toast.LENGTH_LONG).show()
+        }
+
         return binding.root
     }
 
@@ -102,10 +110,7 @@ class FilterFragment : Fragment(), DataLoadingListener {
                 } else {
                     promptDialogPermission()
                 }
-
             }
-
-
         }
     }
 
--- app/src/main/java/com/oreakintobi/oreakintobi/utils/Exceptions.kt
+++ app/src/main/java/com/oreakintobi/oreakintobi/utils/Exceptions.kt
@@ -9,3 +9,4 @@ class ApiException(message: String) : IOException(message)
 // Exception class created to handle no internet status
 // will reflect in NetworkConnectionInterceptor, MyApi and AuthViewModel Classes
 class NoInternetException(message: String) : IOException(message)
+
